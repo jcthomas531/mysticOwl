e33a86de78036ec7bd573befc708dfef6faece87
James C. Thomas <jcthomas531@gmail.com>
2025-09-25 16:57:16 -0500

    added goodman kruskal gamma test

---
+++ R/gkGammaTest.R
@@ -0,0 +1,34 @@
+#' gkGammaTest - Goodman Kruskal Gamma Test
+#' @param x  test
+#' @return A list containing values:
+#' \code{resultHolder} placeholder
+#' @details
+#'    need to do the documentation for this one, see the actual code for commented instructions
+#' @examples 
+#' #test
+#' @importFrom vcdExtra GKgamma
+#' @export
+gkGammaTest <- function (x) {
+  #x should be a data frame where the first column is the y variable 
+  #and the rest of the columns are the x variables
+  #see catagorical data analysis by agresti, page 88
+  #checked that this ASE is in fact the correct ASE by copying an exampled
+  #from SAS
+  tableHolder <- vector(mode = "list", length = ncol(x)-1)
+  testHolder <- vector(mode = "list", length = ncol(x)-1)
+  resultHolder <- vector(mode = "list", length = ncol(x)-1)
+  for (i in 1:(ncol(x)-1)) {
+    tableHolder[[i]] <- table(x[[i+1]], x[[1]])
+    gammaTest <- vcdExtra::GKgamma(tableHolder[[i]])
+    testHolder[[i]] <- list("gamma" = gammaTest$gamma,
+                            "ASE" = gammaTest$sigma,
+                            "Z" = gammaTest$gamma/gammaTest$sigma,
+                            "p-val" = pnorm(abs(gammaTest$gamma/gammaTest$sigma),
+                                            lower.tail = FALSE)*2
+    )
+    resultHolder[[i]] <- list("contingencyTable" = tableHolder[[i]],
+                              "gammaTest" = testHolder[[i]])
+    names(resultHolder)[i] <- colnames(x)[i+1]
+  }
+  return(resultHolder)
+}
