21d4a881a11ae9c7762daa316f7f9ea6eb4bd335
James C. Thomas <jcthomas531@gmail.com>
2025-09-29 18:01:01 -0500

    renaming formatting function for univariate freq tables

--- R/uniFreqForm.R
+++ R/uniFreqForm.R
@@ -1,4 +1,4 @@
-#' freqFormat - Formatting Univariate Frequency Tables
+#' uniFreqForm - Formatting Univariate Frequency Tables
 #' @param x an output of a frequency table from uniFreq() or unifreq2()
 #' @param varName the desired name of the variable as a string
 #' @param extraText extra text to add into the title
@@ -8,11 +8,12 @@
 #'    with uniFreq: pass into x the item from the list to be formatted, use double bracket subsetting. this is done for one single element of uniFreq output at a time
 #'    can also call names on the uniFreq object to know which number to use in bracket
 #'    for unifreq: to display all of the tables you want, use a for loop in r markdown but make sure that you have results='asis' in the header otherwise it prints a bunch of crap
+#'    THIS SHOULD PROBABLY BE UPDATED
 #' @examples 
 #' #test
 #' @import kableExtra
 #' @export
-freqFormat <- function(x, varName = NULL, extraText = NULL) {
+uniFreqForm <- function(x, varName = NULL, extraText = NULL) {
   #default variable name to the var name in the data
   if (is.null(varName)) {
     varName <- colnames(x)[1]
