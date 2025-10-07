2618b7b55fdc62376ac669e04806406e60a1ea13
James C. Thomas <jcthomas531@gmail.com>
2025-10-07 13:02:04 -0500

    updated formatting to work with new kable constraints

--- R/uniFreqForm.R
+++ R/uniFreqForm.R
@@ -4,11 +4,10 @@
 #' @param extraText extra text to add into the title
 #' @return a formatted univariate frequency table
 #' @details
-#'    function for formatting the frequency tables caclulated with uniFreq and uniFreq2 functions
+#'    function for formatting the frequency tables caclulated with uniFreq and uniFreq2 functions.
 #'    with uniFreq: pass into x the item from the list to be formatted, use double bracket subsetting. this is done for one single element of uniFreq output at a time
 #'    can also call names on the uniFreq object to know which number to use in bracket
 #'    for unifreq: to display all of the tables you want, use a for loop in r markdown but make sure that you have results='asis' in the header otherwise it prints a bunch of crap
-#'    THIS SHOULD PROBABLY BE UPDATED
 #' @examples 
 #' #test
 #' @import kableExtra
@@ -20,14 +19,15 @@ uniFreqForm <- function(x, varName = NULL, extraText = NULL) {
   }
   
   formed <- x |>
-    kable(booktabs=TRUE,
-          #"latex",
-          caption = paste("Frequencies for", varName, "variable,", extraText), 
+    kable(caption = paste("Frequencies for", varName, "variable,", extraText), 
+          longtable = FALSE,
+          format = "latex",
+          booktabs=TRUE,
           col.names = c(varName, "Freq.", "Cum. Freq.", 
                         "Percent", "Cum. Percent"),
           linesep = "",
           align = c("l", "r", "r", "r", "r", "r")) |>
-    kable_styling(latex_options = c("HOLD_position")) |> 
+    kable_styling(latex_options = c("HOLD_position", "scale_down")) |> 
     column_spec(1,bold=TRUE) |>
     row_spec(0, bold = TRUE) 
   return(formed)
